f3e189d9d2e0af9625c061554f6b3b9dd4094f48
felix leveille <fil.leveille@hotmail.com>
2022-01-08 00:08:05 -0500

    Added show, new and create methods

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,9 +1,11 @@
 class DogsController < ApplicationController
   def index
-    @dogs = Dog.all.reject do |planet| # this could definitely be made more efficient with a better SQL querry
-      current_user == planet.users
+    @dogs = Dog.all.reject do |dog| # this could definitely be made more efficient with a better SQL querry
+      current_user == dog.user
     end
   end
+
+
 
 
 end
