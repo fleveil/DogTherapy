361370d05e4a613b601c6f55b70ceb62f9611e95
felix leveille <fil.leveille@hotmail.com>
2022-01-07 19:57:33 -0500

    Added dog instance generation to seed

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,8 +3,9 @@ class Dog < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_one_attached :photo
 
-  validates :name, :breed, :energy, :affection, :size, :hypoallergenic, :address, :description, presence: true
-  validates :description, length: { in: 1..300 }
+  validates :name, :breed, :energy, :affection, :size, :address, :description, presence: true
+  validates :hypoallergenic, inclusion: { in: [true, false] }
+  validates :description, length: { in: 1..600 }
   validates :name, length: { in: 1..15 }
   validates :energy, :affection, :size, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
