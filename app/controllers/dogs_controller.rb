e02e6036a50abecea2b14e5023db683606b7cb0a
felix leveille <fil.leveille@hotmail.com>
2022-01-08 00:18:42 -0500

    Added edit, update, destroy, my_dogs and my_dog methods

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -16,6 +16,7 @@ class DogsController < ApplicationController
   def create
     @dog = Dog.new(dog_params)
     @dog.user = current_user
+# Add coordinates from address to dog instance
     if @dog.save
       redirect_to my_dog_path(@dog)
     else
@@ -23,9 +24,39 @@ class DogsController < ApplicationController
     end
   end
 
+  def edit
+    @dog = Dog.find(params[:id])
+  end
 
+  def update
+    @dog = Dog.find(params[:id])
+# Add coordinates from address to dog instance if address changed
+    if @dog.update(dog_params)
+      redirect_to my_dog_path(@dog)
+    else
+      render :edit
+    end
+  end
 
+  def destroy
+    dog = Dog.find(params[:id])
+    dog.destroy
+    redirect_to my_dogs_path
+  end
 
+  def my_dogs
+    @dogs = current_user.dogs
+  end
 
+  def my_dog
+    @dog = Dog.find(params[:id])
+  end
 
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :energy, :affection,
+                                :size, :hypoallergenic, :description,
+                                :address, :photo)
+  end
 end
