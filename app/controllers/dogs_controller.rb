7b23307588adbf8e2785c5339d96e32789edb49e
felix leveille <fil.leveille@hotmail.com>
2022-01-08 00:08:24 -0500

    Same as last commit

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,6 +5,26 @@ class DogsController < ApplicationController
     end
   end
 
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user = current_user
+    if @dog.save
+      redirect_to my_dog_path(@dog)
+    else
+      render :new
+    end
+  end
+
+
+
 
 
 
