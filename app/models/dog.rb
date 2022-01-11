907c2527e94e9bd10c3fcffed0475112a68a3c76
felix leveille <fil.leveille@hotmail.com>
2022-01-10 23:53:27 -0500

    Added geocoding to dog model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,10 +2,12 @@ class Dog < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
   has_one_attached :photo
+  geocoded_by :address
 
   validates :name, :breed, :energy, :affection, :size, :address, :description, presence: true
   validates :hypoallergenic, inclusion: { in: [true, false] }
   validates :description, length: { in: 1..600 }
   validates :name, length: { in: 1..15 }
   validates :energy, :affection, :size, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  after_validation :geocode, if: :will_save_change_to_address?
 end
