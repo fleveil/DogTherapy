b7e03ea02dbde5c1ce83e42b1c6df29916bbaf4e
felix leveille <fil.leveille@hotmail.com>
2022-01-17 13:43:04 -0500

    Added route for home page search form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @search = Search.new
     @dogs = Dog.first(3)
+  end
+
+  def create_search
+    puts 'it works?'
+    raise
   end
 end
