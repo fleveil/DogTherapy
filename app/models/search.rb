7bf32eff8dfa2971c4cdfaefaa4cca3e21926c81
felix leveille <fil.leveille@hotmail.com>
2022-01-17 13:03:10 -0500

    Added validations to search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,18 @@
 class Search < ApplicationRecord
+  geocoded_by :address
+
+  validates :start_date, :end_date, :address, presence: true
+  validate :start_date_not_in_past
+  validate :end_date_not_before_start_date
+  after_validation :geocode, if: :will_save_change_to_address?
+
+  private
+
+  def start_date_not_in_past
+    errors.add(:start_date, "can't be in the past") if start_date < Date.today
+  end
+
+  def end_date_not_before_start_date
+    errors.add(:end_date, "can't be before start date") if start_date > end_date
+  end
 end
